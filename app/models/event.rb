23e386507233e28d5d8e65468e02d74dc2e603c3
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-01-31 19:41:17 -0300

    add uuid to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'bigdecimal'
+require 'securerandom'
+
 require_relative './event/errors'
 
 # Event entity
@@ -8,7 +10,8 @@ class Event
   TYPE_OPTIONS = %w[deposit withdraw transfer].freeze
   private_constant :TYPE_OPTIONS
 
-  attr_reader :type, :origin, :destination, :amount, :timestamp
+  attr_reader :type, :origin, :destination, :amount,
+              :timestamp, :uuid
 
   def initialize(type:, amount:, destination: nil, origin: nil)
     @type = validate_type(type)
@@ -16,6 +19,7 @@ class Event
     @origin = origin&.to_s
     @amount = BigDecimal(amount.to_s)
     @timestamp = Time.now
+    @uuid = SecureRandom.uuid
   end
 
   private
