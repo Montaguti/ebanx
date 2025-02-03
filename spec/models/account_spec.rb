a9c50151090a0f832e46e92509746de578232483
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-02-03 05:50:39 -0300

    ensuring Account.add_event is responding properly

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,6 +6,11 @@ require_relative '../../app/models/account/errors'
 RSpec.describe Account do
   let(:account_id) { 1 }
 
+  describe 'methods' do
+    subject { Account.new(0) }
+    it { is_expected.to respond_to(:add_event) }
+  end
+
   context 'while initilizing' do
     it 'returns an instance of Account' do
       expect(Account.new(account_id)).to be_an_instance_of(Account)
@@ -15,8 +20,18 @@ RSpec.describe Account do
   context 'after initialized' do
     let(:account) { Account.new(1) }
 
-    it 'has an events array' do
+    it 'has an empty Event array' do
       expect(account.events).to eq([])
+    end
+
+    describe 'when adding an event' do
+      before do
+        account.add_event({ type: 'deposit', amount: 100, destination: 1 })
+      end
+
+      it 'increases the events.count' do
+        expect(account.events.count).to eq(1)
+      end
     end
   end
 end
