82430f4c3f49cf2135c930fdef9763a719861c5e
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-02-03 08:37:59 -0300

    add increasing test coverage

--- spec/storage/in_memory_account_repository_spec.rb
+++ spec/storage/in_memory_account_repository_spec.rb
@@ -4,6 +4,20 @@ require_relative '../../app/storage/in_memory_account_repository'
 
 describe InMemoryAccountRepository do
   let(:account_stub) { instance_double('Account', id: 1, events: []) }
+  let(:account2_stub) { instance_double('Account', id: 2, events: []) }
+  let(:dummy_account_class) do
+    Class.new do
+      attr_reader :id
+
+      def initialize(id)
+        @id = id
+      end
+    end
+  end
+
+  before do
+    stub_const('Account', dummy_account_class)
+  end
 
   subject(:repo) { InMemoryAccountRepository.instance }
 
@@ -36,6 +50,29 @@ describe InMemoryAccountRepository do
       it 'returns an account for an existing id' do
         expect(repo.find_account(account_stub.id)).to eq(account_stub)
       end
+    end
+  end
+
+  describe '#find_or_create_account' do
+    context 'when account doesn\'t exists' do
+      it 'returns a new account' do
+        expect(
+          repo.find_or_create_account(account2_stub.id)
+        ).to be_instance_of(Account)
+      end
+    end
+  end
+
+  describe '#save_account' do
+    let(:event) { { type: 'deposit', amount: 10, destination: account_stub.id } }
+
+    it 'overrides an existing account' do
+      account_stub.events << event
+      repo.save_account(account_stub)
+
+      expect(
+        repo.find_account(account_stub.id).events
+      ).to include(event)
     end
   end
 end
