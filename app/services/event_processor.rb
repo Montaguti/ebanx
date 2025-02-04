d13e7deb926b50b5edb93bf0ee5fbbc3b10fc85a
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-02-04 13:25:53 -0300

    add event_processor with tests

---
+++ app/services/event_processor.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require_relative '../models/account'
+require_relative '../models/account/errors'
+require_relative '../models/event'
+require_relative './balance_calculator/default'
+
+# EventProcessor
+class EventProcessor
+  STRATEGIES = {
+    deposit: lambda { |storage, event, _calculator|
+      account = storage.find_or_create_account(event.destination)
+      account.add_event(event)
+      storage.save_account(account)
+
+      [account]
+    },
+    withdraw: lambda { |storage, event, calculator|
+      account = storage.find_account(event.origin)
+      raise Account::NotFoundError unless account
+
+      balance = calculator.calculate(account.id, account.events)
+      raise Account::InsufficientFundsError if balance < event.amount
+
+      account.add_event(event)
+
+      storage.save_account(account)
+
+      [account]
+    },
+    transfer: lambda { |storage, event, calculator|
+      origin = storage.find_account(event.origin)
+      raise Account::NotFoundError unless origin
+
+      balance = calculator.calculate(origin.id, origin.events)
+      raise Account::InsufficientFundsError if balance < event.amount
+
+      destination = storage.find_or_create_account(event.destination)
+
+      origin.add_event(event)
+      destination.add_event(event)
+
+      storage.save_account(origin)
+      storage.save_account(destination)
+
+      [origin, destination]
+    }
+  }.freeze
+
+  def self.process(storage, event, calculator: BalanceCalculator::Default)
+    strategy = STRATEGIES[event.type]
+    strategy.call(storage, event, calculator)
+  end
+end
---
+++ spec/services/event_processor_spec.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+require_relative '../../app/services/event_processor'
+
+RSpec.describe EventProcessor do
+  let(:storage) { instance_double('AccountRepository') }
+  let(:account) { instance_double('Account', id: 1, events: []) }
+  let(:calculator) { class_double('BalanceCalculator::Default') }
+
+  before do
+    allow(storage).to receive(:find_account).with(account.id).and_return(account)
+    allow(storage).to receive(:save_account)
+    allow(storage).to receive(:find_or_create_account).with(account.id).and_return(account)
+    allow(account).to receive(:add_event)
+  end
+
+  describe '#process' do
+    context 'with :deposit event' do
+      let(:event) { instance_double('Event', type: :deposit, destination: account.id, amount: 10) }
+
+      it 'searchs the account and stores the event' do
+        expect(storage).to receive(:find_or_create_account).with(event.destination)
+        expect(account).to receive(:add_event).with(event)
+        expect(storage).to receive(:save_account).with(account)
+
+        result = described_class.process(storage, event)
+        expect(result).to eq([account])
+      end
+    end
+
+    context 'with :transfer event' do
+      let(:origin) { double('Account', id: 3, events: []) }
+      let(:event) do
+        instance_double('Event', type: :transfer, destination: account.id, origin: origin.id, amount: 10)
+      end
+
+      context 'when the origin doesn\'t exists' do
+        before do
+          allow(storage).to receive(:find_account).with(origin.id).and_return(nil)
+        end
+
+        it 'raises Account::NotFoundError' do
+          expect { described_class.process(storage, event) }.to raise_error(Account::NotFoundError)
+        end
+      end
+
+      context 'when the origin doesn\'t has funds' do
+        before do
+          allow(storage).to receive(:find_account).with(origin.id).and_return(origin)
+        end
+
+        it 'rasies Account::InsufficientFundsError' do
+          expect do
+            described_class.process(storage, event)
+          end.to raise_error(Account::InsufficientFundsError)
+        end
+      end
+
+      context 'with sufficient funds' do
+        before do
+          allow(calculator).to receive(:calculate).with(origin.id, origin.events).and_return(20)
+        end
+
+        it 'trasnfer funds between accounts' do
+          expect(storage).to receive(:find_account).with(origin.id).and_return(origin)
+          expect(storage).to receive(:find_or_create_account).with(account.id)
+          expect(origin).to receive(:add_event).with(event)
+          expect(account).to receive(:add_event).with(event)
+
+          described_class.process(storage, event, calculator: calculator)
+        end
+      end
+    end
+
+    context 'with :withdraw event' do
+      let(:event) { instance_double('Event', type: :withdraw, origin: account.id, amount: 10) }
+
+      context 'when account doesn\'t exists' do
+        before do
+          allow(storage).to receive(:find_account).with(event.origin).and_return(nil)
+        end
+
+        it 'raises Account::NotFoundError' do
+          expect { described_class.process(storage, event) }.to raise_error(Account::NotFoundError)
+        end
+      end
+
+      context 'when account doesn\'t has funds' do
+        it 'raises Account::InsufficientFundsError' do
+          expect { described_class.process(storage, event) }.to raise_error(Account::InsufficientFundsError)
+        end
+      end
+
+      context 'with sufficient funds' do
+        before do
+          allow(calculator).to receive(:calculate).with(event.origin, account.events).and_return(100)
+        end
+
+        it 'add a subtraction event to account' do
+          expect(account).to receive(:add_event)
+          expect(storage).to receive(:save_account).with(account)
+
+          described_class.process(storage, event, calculator: calculator)
+        end
+      end
+    end
+  end
+end
